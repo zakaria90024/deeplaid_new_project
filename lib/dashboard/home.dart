d635e19cf4068d7db38b13bf63581eba1e43b6d0
Md. Zakaria Ahnaf <zakaria112203@gmail.com>
2023-03-05 17:03:36 +0600

    added Localization - Bangla/English, color changed, color,

--- lib/dashboard/home.dart
+++ lib/dashboard/home.dart
@@ -15,6 +15,7 @@ class _HomeState extends State<Home> {
       width: 100,
       child:Container(
         color: Colors.blue,
+        //write here Sizebox, Container , Column
       ) ,
     );
   }
